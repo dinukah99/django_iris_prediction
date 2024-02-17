76a073a9130f737020cb19473ea3067724fb5c1c
dinukah99 <dinukahamarasinghe@gmail.com>
2024-02-17 18:10:32 +0530

    predict & predict chances views

--- predict/views.py
+++ predict/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import pandas as pd
 
 
 def predict(request):
     return render(request, 'predict.html')
+
+
+def predict_chances(request):
+    if request.POST.get('action') == 'post':
+        # Receives the input from the form
+        sepal_length = float(request.POST.get('sepal_length'))
+        sepal_width = float(request.POST.get('sepal_width'))
+        petal_length = float(request.POST.get('petal_length'))
+        petal_width = float(request.POST.get('petal_width'))
+
+        # Unpickle the model
+        model = pd.read_pickle(r"C:\Users\Dinuka\Downloads\Iris Predictor\new_model.pickle")
+
+        # Make prediction
+        result = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+
+        classification = result[0]
+
+        return JsonResponse({'result': classification, 'sepal_length': sepal_length, 'sepal_width': sepal_width,
+                             'petal_length': petal_length, 'petal_width': petal_width}, safe=False)
+    else:
+        return JsonResponse({'error': 'Invalid request'}, status=400)
